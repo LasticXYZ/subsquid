c76b3cf21a49d8acda4ba8fe340f3829aee5fd29
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-05-03 12:56:04 +0200

    dependencies

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 # 1. Install dependencies
 npm ci
 
+npx sqd down
+
 # 2. Start target Postgres database and detach
 npx sqd up 
 
 # 3. Build the project
 npx sqd build
 
+npx sqd migration:generate
 npx sqd migration:apply
+
 # 4. Start both the squid processor and the GraphQL server does both sqd serve and 
 npx sqd run .
